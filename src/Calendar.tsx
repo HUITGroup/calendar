4619192664add20bb627510254929330b6596980
manattan <mogimogi0529@eis.hokudai.ac.jp>
2021-03-12 22:05:29 +0900

    イベント追加フォーム無くしちゃって、既存データ引っ張ってくるところまで

--- src/Calendar.tsx
+++ src/Calendar.tsx
@@ -1,114 +1,30 @@
-import React, { useState } from 'react';
-import { Button, Modal, Input } from '@material-ui/core'
-import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import React, { useState, useEffect } from 'react';
 import FullCalendar from '@fullcalendar/react';
 import interactionPlugin from '@fullcalendar/interaction';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import dayGridPlugin from '@fullcalendar/daygrid';
-import { useSpring, animated } from 'react-spring'
 import styled from 'styled-components';
 import firebase from './lib/firebase'
 import 'firebase/firestore'
+import * as Types from './Types/main'
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    modal: {
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    paper: {
-      backgroundColor: theme.palette.background.paper,
-      border: '2px solid #000',
-      boxShadow: theme.shadows[5],
-      padding: theme.spacing(2, 4, 3),
-    },
-  }),
-);
-
-interface FadeProps {
-  children?: React.ReactElement;
-  in: boolean;
-  onEnter?: () => {};
-  onExited?: () => {};
-}
-
-const Fade = React.forwardRef<HTMLDivElement, FadeProps>(function Fade(props, ref) {
-  const { in: open, children, onEnter, onExited, ...other } = props;
-  const style = useSpring({
-    from: { opacity: 0 },
-    to: { opacity: open ? 1 : 0 },
-    onStart: () => {
-      if (open && onEnter) {
-        onEnter();
-      }
-    },
-    onRest: () => {
-      if (!open && onExited) {
-        onExited();
-      }
-    },
-  });
-
-  return (
-    <animated.div ref={ref} style={style} {...other}>
-      {children}
-    </animated.div>
-  );
-});
-
-// とりあえず form で登録しているが、今後discordのchatから登録させるようにする
 const Calendar: React.FC = () => {
-  const event = [
-    {
-      title: 'Lunch',
-      start: '2021-03-20T13:00:00',
-      constraint: 'business'
-    },
-    {
-      title: 'Lunch33',
-      start: '2021-03-25T09:00:00',
-      constraint: 'business'
-    },
-  ]
-  const classes = useStyles();
-  const [events, setEvents] = useState(event)
-  const [opened, setOpened] = useState<boolean>(false)
-
-  const [title, setTitle] = useState("")
-  const [start, setStart] = useState("")
-  const [constraint, setConstraint] = useState("")
-
-  const add = () => {
-    setOpened(true)
-  }
-
-  const setData = () => {
-    const newD = {
-      title,
-      start,
-      constraint
+  useEffect(() => {
+    async function getInitData() {
+      const db = firebase.firestore()
+      const ref = db.collection('/data/')
+      const qs = await ref.get()
+      const tmp: any = qs.docs.map(doc => Object.assign(doc.data(), { id: doc.id }))
+      setEvents(tmp)
     }
-    console.log(newD)
-    const tmp = events
-    tmp.push(newD)
-    console.log(tmp)
-    setEvents(tmp)
-  }
+
+    getInitData()
+  }, [])
+
+  const [events, setEvents] = useState<Types.event[] | undefined>(undefined)
 
   return (
     <CalendarDiv>
-      <Button onClick={add}>追加する</Button>
-      <Modal className={classes.modal} open={opened} onClose={() => setOpened(false)}>
-        <Fade in={opened}>
-          <div className={classes.paper}>
-            <Input onChange={(event: React.ChangeEvent<any>) => setTitle(event.target.value)}></Input>
-            <Input type="date" onChange={(event: React.ChangeEvent<any>) => setStart(event.target.value)}></Input>
-            <Input onChange={(event: React.ChangeEvent<any>) => setConstraint(event.target.value)}></Input>
-            <Button onClick={setData}>追加する</Button>
-          </div>
-        </Fade>
-      </Modal>
       <FullCalendar
         locale='ja'
         plugins={[timeGridPlugin, dayGridPlugin, interactionPlugin]}
